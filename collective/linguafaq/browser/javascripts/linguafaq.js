100337ba22faeb692542b44749a227c52c293ef3
khink <khink@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-11-03 14:04:56 +0000

    remove Was This Useful functionality (has gone into collective.wasthisuseful)

--- collective/linguafaq/browser/javascripts/linguafaq.js
+++ collective/linguafaq/browser/javascripts/linguafaq.js
@@ -1,11 +1,5 @@
 var linguafaq = {
 
-    expandForm: function() {
-        $('#useful input:radio[value="0"]').click(function() {
-            jq(this).parent().find(".comments").fadeIn();
-        }); 
-    },
-
     expandFaqItemAnswer: function() {
         $('a.faqItemLink').prepOverlay({
             subtype: 'ajax',
@@ -16,6 +10,5 @@ var linguafaq = {
 
 jq(document).ready(function() {
     linguafaq.expandFaqItemAnswer();
-    linguafaq.expandForm();
 });
 
--- collective/linguafaq/browser/viewlets.py
+++ collective/linguafaq/browser/viewlets.py
@@ -3,19 +3,13 @@ from zope.component import getMultiAdapter
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.layout.viewlets.common import ViewletBase
 
-class WasThisUsefulViewlet(ViewletBase):
-    render = ViewPageTemplateFile('useful.pt')
-
-    def update(self):
-        pass
-
 class LinkToContactFormViewlet(ViewletBase):
     render = ViewPageTemplateFile('linktocontactform.pt')
 
     def update(self):
-        self.contact_action = self._getContentAction()
+        self.contact_action = self._getContactAction()
         
-    def _getContentAction(self):
+    def _getContactAction(self):
         context_state = getMultiAdapter((self.context, self.request),
                                         name=u'plone_context_state')
         site_actions = context_state.actions('site_actions')
